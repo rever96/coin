d59ebcb3b76ced148193277d219e4f4ccb0ebf22
Loris <rever22411@gmail.com>
2019-10-21 12:55:44 +0200

    funzioni creazione piano abbonamento stripe

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
 //Install express server
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg');
+// const { Pool, Client } = require('pg');
 // const client = new Client();
 // client.connect();
 // const sql = `SELECT 1 AS "\\'/*", 2 AS "\\'*/\n + console.log(process.env)] = null;\n//"`;
 // client.query(sql, (err, res) => {
 //   client.end();
 // });
-const pool = new Pool({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211
-});
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-const client = new Client({
-  user: 'dbuser',
-  host: 'database.server.com',
-  database: 'mydb',
-  password: 'secretpassword',
-  port: 3211
-});
-client.connect();
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-  client.end();
-});
+// const pool = new Pool({
+//   user: 'dbuser',
+//   host: 'database.server.com',
+//   database: 'mydb',
+//   password: 'secretpassword',
+//   port: 3211
+// });
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+// const client = new Client({
+//   user: 'dbuser',
+//   host: 'database.server.com',
+//   database: 'mydb',
+//   password: 'secretpassword',
+//   port: 3211
+// });
+// client.connect();
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res);
+//   client.end();
+// });
 const app = express();
 
 app.use((req, res, next) => {
@@ -55,3 +55,33 @@ app.post('/prova', (req, res) => {
 app.listen(8080, () => {
   console.log('online');
 });
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const stripeKeyPublic = 'pk_test_rF1OyNuizo6EhmqMHcWpDBGd00LhGH7BDi';
+const stripeKeyPrivate = 'sk_test_LSVOpfM5vkb9ePoIDkjRKpdl00dGeypAfp';
+
+const stripe = require('stripe')(stripeKeyPrivate);
+
+// (async () => {
+//   console.log(
+//     'creazione api che rappresenta il servizio che si vuole offrire al cliente'
+//   );
+//   const product = await stripe.products.create({
+//     name: 'My SaaS Platform',
+//     type: 'service'
+//   });
+// })();
+
+// (async () => {
+//   console.log('crea un piano relativo al prodotto specificato');
+//   const plan = await stripe.plans
+//     .create({
+//       product: 'prod_G21IvcIuBSYAdu',
+//       nickname: 'SaaS Platform USD',
+//       currency: 'eur',
+//       interval: 'year',
+//       amount: 100
+//     })
+//     .then(obj => console.log(obj));
+// })();
