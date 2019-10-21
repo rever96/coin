74fe60726e3dc8c5c97607182a18e31313dfee50
Loris <rever22411@gmail.com>
2019-10-21 10:15:10 +0200

    prova sql query server

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 //Install express server
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool, Client } = require('pg');
+// const client = new Client();
+// client.connect();
+// const sql = `SELECT 1 AS "\\'/*", 2 AS "\\'*/\n + console.log(process.env)] = null;\n//"`;
+// client.query(sql, (err, res) => {
+//   client.end();
+// });
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211
+});
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211
+});
+client.connect();
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
+});
 const app = express();
 
 app.use((req, res, next) => {
