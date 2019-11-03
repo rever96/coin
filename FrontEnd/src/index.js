2d4d588ba5ab36d7baaf8c7b3c2baf1aba0b89d5
Loris <rever22411@gmail.com>
2019-11-03 14:40:46 +0100

    fix errore require history

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -6,13 +6,9 @@ import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
--- FrontEnd/src/redux/reducers/app/index.js
+++ FrontEnd/src/redux/reducers/app/index.js
@@ -1,18 +1,18 @@
 import { APP_TOOGLE_SIDEBAR } from '../../actions/app';
 
 const initialState = {
-    isSidebarHidden: true,
+  isSidebarHidden: false
 };
 
 function toogleSidebar(state) {
-    return { isSidebarHidden: !state.isSidebarHidden };
+  return { isSidebarHidden: !state.isSidebarHidden };
 }
 
 export default function dashboardData(state = initialState, action) {
-    switch (action.type) {
-        case APP_TOOGLE_SIDEBAR:
-            return toogleSidebar(state);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case APP_TOOGLE_SIDEBAR:
+      return toogleSidebar(state);
+    default:
+      return state;
+  }
 }
