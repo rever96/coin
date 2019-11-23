1fa81407f34872c7fb100f19329edff0ed93daa6
Loris <rever22411@gmail.com>
2019-11-23 11:49:42 +0100

    add/delete row

--- front-end/src/components/edi_table/edi_table.js
+++ front-end/src/components/edi_table/edi_table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './edi_table.css';
-import { Table, Input, InputNumber, Popconfirm, Form } from 'antd';
+import { Table, Input, InputNumber, Popconfirm, Form, Button } from 'antd';
 
 const EditableContext = React.createContext();
 
@@ -55,7 +55,11 @@ class EditableTable extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
-    this.state = { data: props.righe, editingKey: '' };
+    this.state = {
+      dataSource: props.righe,
+      editingKey: '',
+      count: props.righe[props.righe.length - 1].key + 1
+    };
     this.columns = props.colonne;
     this.columns.push({
       title: 'operation',
@@ -68,6 +72,7 @@ class EditableTable extends React.Component {
             <EditableContext.Consumer>
               {form => (
                 <p
+                  className="link"
                   onClick={() => this.save(form, record.key)}
                   style={{ marginRight: 8 }}
                 >
@@ -75,17 +80,26 @@ class EditableTable extends React.Component {
                 </p>
               )}
             </EditableContext.Consumer>
-            <Popconfirm
-              title="Sure to cancel?"
-              onConfirm={() => this.cancel(record.key)}
-            >
-              <p>Cancel</p>
-            </Popconfirm>
+            <p className="link" onClick={() => this.cancel(record.key)}>
+              Cancel
+            </p>
           </span>
         ) : (
-          <p disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>
-            Edit
-          </p>
+          <>
+            <p
+              className="link"
+              disabled={editingKey !== ''}
+              onClick={() => this.edit(record.key)}
+            >
+              Edit
+            </p>
+            <Popconfirm
+              title="Non potrai annullare questa azione, continuare?"
+              onConfirm={() => this.handleDelete(record.key)}
+            >
+              <p className="link">Delete</p>
+            </Popconfirm>
+          </>
         );
       }
     });
@@ -97,12 +111,31 @@ class EditableTable extends React.Component {
     this.setState({ editingKey: '' });
   };
 
+  handleDelete = key => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
+  };
+
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    let emptyRow = {};
+    this.props.colonne.forEach(c => {
+      emptyRow[c.title] = '';
+    });
+    emptyRow.key = count;
+    this.setState({
+      dataSource: [emptyRow, ...dataSource],
+      count: count + 1
+    });
+    this.edit(emptyRow.key);
+  };
+
   save(form, key) {
     form.validateFields((error, row) => {
       if (error) {
         return;
       }
-      const newData = [...this.state.data];
+      const newData = [...this.state.dataSource];
       const index = newData.findIndex(item => key === item.key);
       if (index > -1) {
         const item = newData[index];
@@ -110,10 +143,10 @@ class EditableTable extends React.Component {
           ...item,
           ...row
         });
-        this.setState({ data: newData, editingKey: '' });
+        this.setState({ dataSource: newData, editingKey: '' });
       } else {
         newData.push(row);
-        this.setState({ data: newData, editingKey: '' });
+        this.setState({ dataSource: newData, editingKey: '' });
       }
     });
   }
@@ -146,11 +179,18 @@ class EditableTable extends React.Component {
 
     return (
       <EditableContext.Provider value={this.props.form}>
+        <Button
+          onClick={this.handleAdd}
+          type="primary"
+          style={{ marginBottom: 16 }}
+        >
+          Aggiungi riga
+        </Button>
         <Table
-          scroll={{ x: '2000' }}
+          scroll={{ x: 2000 }}
           components={components}
           bordered
-          dataSource={this.state.data}
+          dataSource={this.state.dataSource}
           columns={columns}
           rowClassName="editable-row"
           pagination={{
