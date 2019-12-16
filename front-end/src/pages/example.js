8177a1b5faaea351417f4327ecd4fd194c8b832b
Loris <rever22411@gmail.com>
2019-12-16 14:49:32 +0100

    basic

--- front-end/src/pages/example.js
+++ front-end/src/pages/example.js
@@ -1,144 +1,49 @@
 import React from 'react';
-import { Table, Input, Button, Icon } from 'antd';
-import Highlighter from 'react-highlight-words';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park'
-  },
-  {
-    key: '2',
-    name: 'Joe Black',
-    age: 42,
-    address: 'London No. 1 Lake Park'
-  },
-  {
-    key: '3',
-    name: 'Jim Green',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park'
-  },
-  {
-    key: '4',
-    name: 'Jim Red',
-    age: 32,
-    address: 'London No. 2 Lake Park'
-  }
-];
+const localizer = momentLocalizer(moment);
 
 class Example extends React.Component {
-  state = {
-    searchText: '',
-    searchedColumn: ''
-  };
-
-  getColumnSearchProps = dataIndex => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters
-    }) => (
-      <div style={{ padding: 8 }}>
-        <Input
-          ref={node => {
-            this.searchInput = node;
-          }}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={e =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() =>
-            this.handleSearch(selectedKeys, confirm, dataIndex)
-          }
-          style={{ width: 188, marginBottom: 8, display: 'block' }}
-        />
-        <Button
-          type="primary"
-          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
-          icon="search"
-          size="small"
-          style={{ width: 90, marginRight: 8 }}
-        >
-          Search
-        </Button>
-        <Button
-          onClick={() => this.handleReset(clearFilters)}
-          size="small"
-          style={{ width: 90 }}
-        >
-          Reset
-        </Button>
-      </div>
-    ),
-    filterIcon: filtered => (
-      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes(value.toLowerCase()),
-    onFilterDropdownVisibleChange: visible => {
-      if (visible) {
-        setTimeout(() => this.searchInput.select());
-      }
-    },
-    render: text =>
-      this.state.searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[this.state.searchText]}
-          autoEscape
-          textToHighlight={text.toString()}
-        />
-      ) : (
-        text
-      )
-  });
-
-  handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    this.setState({
-      searchText: selectedKeys[0],
-      searchedColumn: dataIndex
+  constructor() {
+    super();
+    this.events = [...Array(10).keys()].map(index => {
+      return {
+        id: index,
+        title: 'evento ' + index,
+        start: moment(
+          moment.now() + index * 3 * 3600 * 1000 - 9600000
+        ).toDate(),
+        end: moment(moment.now() + index * 3 * 3600 * 1000).toDate()
+      };
     });
-  };
+    this.events.push({
+      id: -1,
+      title: 'evento su più giorni',
+      start: moment(moment.now()).toDate(),
+      end: moment(moment.now() + 36 * 3600 * 1000).toDate(),
+      example: 'omg'
+    });
+  }
 
-  handleReset = clearFilters => {
-    clearFilters();
-    this.setState({ searchText: '' });
-  };
+  selectEvent(e) {
+    console.log(e);
+  }
 
   render() {
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        width: '30%',
-        ...this.getColumnSearchProps('name')
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-        width: '20%',
-        ...this.getColumnSearchProps('age')
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-        ...this.getColumnSearchProps('address')
-      }
-    ];
-    console.log(this.state.searchedColumn);
-    return <Table columns={columns} dataSource={data} />;
+    console.log(this.events);
+    return (
+      <Calendar
+        localizer={localizer}
+        events={this.events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        defaultView="week"
+        onSelectEvent={this.selectEvent}
+      />
+    );
   }
 }
 
