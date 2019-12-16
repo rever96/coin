00d9c41c8273b47c0b44401f7ad9d68fa096a6a7
Loris <rever22411@gmail.com>
2019-12-16 17:18:45 +0100

    dnd (bugged)

--- front-end/src/pages/example.js
+++ front-end/src/pages/example.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 
 const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
+
+class ExampleDnD extends React.Component {
+  state = {
+    events: [
+      {
+        start: new Date(),
+        end: moment(moment.now() + 24 * 3600 * 1000),
+        title: 'Some title'
+      }
+    ]
+  };
+
+  onEventResize = ({ event, start, end, allDay }) => {
+    console.log(end);
+
+    this.setState({
+      events: [
+        {
+          start: start,
+          end: end,
+          title: 'Some title'
+        }
+      ]
+    });
+  };
+
+  onEventDrop = ({ event, start, end, allDay }) => {
+    console.log(end);
+    this.setState({
+      events: [
+        {
+          start: start,
+          end: end,
+          title: 'Some title'
+        }
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <DnDCalendar
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          localizer={localizer}
+          onEventDrop={this.onEventDrop.bind(this)}
+          onEventResize={this.onEventResize.bind(this)}
+          resizable
+          style={{ height: '500px' }}
+        />
+      </div>
+    );
+  }
+}
 
 class Example extends React.Component {
   constructor() {
@@ -34,15 +93,20 @@ class Example extends React.Component {
   render() {
     console.log(this.events);
     return (
-      <Calendar
-        localizer={localizer}
-        events={this.events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        defaultView="week"
-        onSelectEvent={this.selectEvent}
-      />
+      <>
+        <Calendar
+          localizer={localizer}
+          events={this.events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          defaultView="week"
+          onSelectEvent={this.selectEvent}
+        />
+        <hr></hr>
+        <hr></hr>
+        <ExampleDnD></ExampleDnD>
+      </>
     );
   }
 }
