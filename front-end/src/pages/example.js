169cf5cd95d42d4c3a2bd79ba134671dd1ad0131
Loris <rever22411@gmail.com>
2019-12-19 11:32:50 +0100

    drag-drop fixed on week

--- front-end/src/pages/example.js
+++ front-end/src/pages/example.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 
-const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
+const localizer = BigCalendar.momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(BigCalendar);
 
 class ExampleDnD extends React.Component {
-  state = {
-    events: [
-      {
-        start: new Date(),
-        end: moment(moment.now() + 24 * 3600 * 1000),
-        title: 'Some title'
-      }
-    ]
-  };
+  constructor() {
+    super();
+    const events = [...Array(10).keys()].map(index => {
+      return {
+        id: index,
+        title: 'evento ' + index,
+        start: moment(
+          moment.now() + index * 3 * 3600 * 1000 - 9600000
+        ).toDate(),
+        end: moment(moment.now() + index * 3 * 3600 * 1000).toDate()
+      };
+    });
+    events.push({
+      id: -1,
+      title: 'evento su più giorni',
+      start: moment(moment.now()).toDate(),
+      end: moment(moment.now() + 36 * 3600 * 1000).toDate(),
+      example: 'omg'
+    });
+    this.state = {
+      events
+    };
+  }
 
   onEventResize = ({ event, start, end, allDay }) => {
     console.log(end);
@@ -51,7 +65,7 @@ class ExampleDnD extends React.Component {
       <div className="App">
         <DnDCalendar
           defaultDate={new Date()}
-          defaultView="month"
+          defaultView="week"
           events={this.state.events}
           localizer={localizer}
           onEventDrop={this.onEventDrop.bind(this)}
@@ -64,51 +78,4 @@ class ExampleDnD extends React.Component {
   }
 }
 
-class Example extends React.Component {
-  constructor() {
-    super();
-    this.events = [...Array(10).keys()].map(index => {
-      return {
-        id: index,
-        title: 'evento ' + index,
-        start: moment(
-          moment.now() + index * 3 * 3600 * 1000 - 9600000
-        ).toDate(),
-        end: moment(moment.now() + index * 3 * 3600 * 1000).toDate()
-      };
-    });
-    this.events.push({
-      id: -1,
-      title: 'evento su più giorni',
-      start: moment(moment.now()).toDate(),
-      end: moment(moment.now() + 36 * 3600 * 1000).toDate(),
-      example: 'omg'
-    });
-  }
-
-  selectEvent(e) {
-    console.log(e);
-  }
-
-  render() {
-    console.log(this.events);
-    return (
-      <>
-        <Calendar
-          localizer={localizer}
-          events={this.events}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 500 }}
-          defaultView="week"
-          onSelectEvent={this.selectEvent}
-        />
-        <hr></hr>
-        <hr></hr>
-        <ExampleDnD></ExampleDnD>
-      </>
-    );
-  }
-}
-
-export default Example;
+export default ExampleDnD;
